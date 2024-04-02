a7bb632f05aeeac05cc55e42694737dc3aa92e2e
duckien2012 <hoanganhtra181@gmail.com>
2024-04-02 22:37:30 +0700

    Update databse + CS2

--- ATBM_NHOM12/Proc/database.sql
+++ ATBM_NHOM12/Proc/database.sql
@@ -11,27 +11,27 @@ DROP TABLE "SYS"."SINHVIEN" CASCADE CONSTRAINTS;
 
 create table NHANSU 
 (
-    MANV varchar(5),
+    MANV varchar(6),
     HOTEN varchar(30),
     PHAi varchar(3),
     NGSINH date,
     PHUCAP float,
     ĐT char(10), --
     VAITRO varchar(30),
-    MAĐV varchar(5), --
+    MAĐV varchar(6), --
     primary key(MANV)
 )
 /
 create table SINHVIEN
 (
-    MASV varchar(5),
+    MASV varchar(6),
     HOTEN varchar(30),
     PHAI varchar(3),
     NGSINH date,
     ĐCHI varchar(50),
     ĐT char(5), -- 
-    MACT varchar(5),
-    MANGANH varchar(5),
+    MACT varchar(6),
+    MANGANH varchar(6),
     SOTCTL int,
     ĐTBTL float, --
     primary key(MASV)
@@ -39,52 +39,51 @@ create table SINHVIEN
 /
 create table ĐONVI 
 (
-    MAĐV varchar(5), 
+    MAĐV varchar(6), 
     TENĐV varchar(50),
-    TRGĐV varchar(5),
+    TRGĐV varchar(6),
     primary key(MAĐV) 
 )
 /
 create table HOCPHAN
 (
-    MAHP varchar(5),
+    MAHP varchar(6),
     TENHP varchar(50),
     SOTC int,
     STLT int,
     STTH int,
     SOSVĐT int,
-    MAĐV varchar(5),
+    MAĐV varchar(6),
     primary key(MAHP)
 )
 /
 create table KHMO 
 (
-    MAHP varchar(5),
+    MAHP varchar(6),
     HK int,
     NAM int,
-    NGBATDAU date,
-    MACT varchar(5),
+    MACT varchar(6),
     primary key(MAHP, HK, NAM, MACT)
 )
 /
 create table PHANCONG
 (
-    MAGV varchar(5),
-    MAHP varchar(5),
+    MAGV varchar(6),
+    MAHP varchar(6),
     HK int,
     NAM int,
-    MACT varchar(5),
+    MACT varchar(6),
     primary key (MAGV, MAHP, HK, NAM, MACT)
 )
 /
 create table ĐANGKY
 (
-    MASV varchar(5),
-    MAGV varchar(5),
-    MAHP varchar(5),
+    MASV varchar(6),
+    MAGV varchar(6),
+    MAHP varchar(6),
     HK int,
     NAM int,
-    MACT varchar(5),
+    MACT varchar(6),
     ĐIEMTHI float,
     ĐIEMQT float,
     ĐIEMCK float,
--- ATBM_NHOM12/Proc/view.sql
+++ ATBM_NHOM12/Proc/view.sql
@@ -1,12 +1,23 @@
+ALTER SESSION SET "_ORACLE_SCRIPT" = True;
+create role RL_NVCOBAN;
+create role RL_GIANGVIEN;
+create role RL_GIAOVU;
+create role RL_TRUONGDV;
+create role RL_TRUONGKHOA;
 -- CS1: Nhân viên cơ bản
 create or replace view QLHS_TTCANHAN
 as
     select * from NHANSU where MANV = SYS_CONTEXT('USERENV','SESSION_USER');
 -- Grant update on view
+grant select, update(ĐT) on QLHS_TTCANHAN to RL_NVCOBAN;
 -- Grant select on sinh vien, don vi, hoc phan, khmo
-
+grant select on SINHVIEN to RL_NVCOBAN;
+grant select on ĐONVI to RL_NVCOBAN;
+grant select on HOCPHAN to RL_NVCOBAN;
+grant select on KHMO to RL_NVCOBAN;
 -- CS2 : Giảng viên
 -- view QLHS_TTCANHAN
+grant select, update(ĐT) on QLHS_TTCANHAN to RL_GIANGVIEN;
 -- grant select on QLHS_PHANCONG_GD
 -- grant select on QLHS_DANGKY_HPGD
 create or replace view QLHS_PHANCONG_GD
@@ -16,8 +27,10 @@ as
 create or replace view QLHS_DANGKY_HPGD
 as
     select * from ĐANGKY where MAGV = SYS_CONTEXT('USERENV','SESSION_USER');
+grant select on QLHS_PHANCONG_GD to RL_GIANGVIEN;
+grant select on QLHS_DANGKY_HPGD to RL_GIANGVIEN;
 -- grant update on dangky(diemth,diemqt, diemck,diemtk)
-
+/
 --CS3 : giao vu
 -- view QLHS_TTCANHAN
 -- grant update select insert on sinh vien, donvi, hocphan, khmo
@@ -27,19 +40,7 @@ create or replace view QLHS_PHANCONG_HOCPHAN_VPK
 as
     select pc.* from PHANCONG pc, HOCPHAN hp where pc.mađv = hp.mađv and hp.MAĐV = 'VPK';
     
-CREATE OR REPLACE TRIGGER check_ThoiHan_HieuChinh
-BEFORE INSERT OR DELETE ON ĐANGKY
-FOR EACH ROW
-DECLARE
-    start_date DATE;
-BEGIN
-    SELECT NGBATDAU INTO start_date FROM KHMO
-    WHERE MAHP = :NEW.MAHP AND HK = :NEW.HK AND NAM = :NEW.NAM;
-    
-    IF SYSDATE > start_date + 14 THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Không thể hiệu chỉnh học phần vì đã quá thời hạn 14 ngày!');
-    END IF;
-END;
+
 /
 -- CS4 : TRưởng đơn vị
 -- view QLHS_TTCANHAN
@@ -133,5 +134,5 @@ END;
 -- Thêm, Xóa các dòng dữ liệu đăng ký học phần (ĐANGKY) liên quan đến chính sinh
 --viên đó trong học kỳ của năm học hiện tại (nếu thời điểm hiệu chỉnh đăng ký còn hợp
 --lệ).
-
+ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE;
     
