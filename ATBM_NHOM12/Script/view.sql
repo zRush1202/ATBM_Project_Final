a1d12ccfaf02187fe1d0ee1a4f3b998a6789d76f
duckien2012 <hoanganhtra181@gmail.com>
2024-04-08 13:22:23 +0700

    update cs3,6

--- ATBM_NHOM12/Script/view.sql
+++ ATBM_NHOM12/Script/view.sql
@@ -77,20 +77,18 @@ BEGIN
   FROM DBA_ROLE_PRIVS
   WHERE GRANTEE = USERNAME;
   IF 'RL_GIAOVU' IN (USERROLE) THEN 
-      OPEN CUR; 
-      STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE'; 
-      EXECUTE IMMEDIATE(STRSQL);  
+      OPEN CUR;  
         LOOP 
             FETCH HP INTO TEMP; 
             EXIT WHEN HP%NOTFOUND;
-            IF MAHP IS NOT NULL THEN
-                MAHP := MAHP || ',''' || TEMP || '''';
+            IF (MAHP IS NOT NULL) THEN
+                MAHP :=  MAHP ||''',''';
             ELSE 
-                MAHP := '''' || TEMP || '''';
+                MAHP := MAHP || TEMP;
             END IF;
         END LOOP;
         CLOSE HP;
-        RETURN 'MAHP IN (' || MAHP || ')';
+        RETURN 'MAHP IN ('''||MAHP||''')';
   ELSE
     RETURN '1=0';
   END IF;
@@ -98,15 +96,62 @@ END;
 /
 BEGIN
     DBMS_RLS.ADD_POLICY(
-        object_schema => 'ADMIN_HW4', 
+        object_schema => 'ADPRO', 
         object_name => 'PHANCONG',
-        policy_name => 'PHANCONG_CONTROL_CS3',
+        policy_name => 'GIAOVU_PHANCONG_CS3',
         policy_function => 'QL_XEM_HP_VPK',
         statement_types => 'UPDATE',
         update_check => TRUE
     );
 END;
 /
+CREATE OR REPLACE TRIGGER CheckRegistrationDate
+INSTEAD OF INSERT OR DELETE ON DANGKY
+FOR EACH ROW
+DECLARE
+    v_StartDate DATE;
+    v_CurrentDate DATE := SYSDATE;
+    v_HK INT;
+    v_NAM INT;
+BEGIN
+    SELECT GRANTED_ROLE INTO USERROLE
+    FROM DBA_ROLE_PRIVS
+    WHERE GRANTEE = SYS_CONTEXT('USERENV', 'SESSION_USER');
+    IF 'RL_GIAOVU' IN (USERROLE) THEN 
+        -- Get the semester start date based on the academic year and semester
+        SELECT TO_DATE(
+            CASE HK
+                WHEN 1 THEN TO_CHAR(NAM) || '-01-01'
+                WHEN 2 THEN TO_CHAR(NAM) || '-05-01'
+                WHEN 3 THEN TO_CHAR(NAM) || '-09-01'
+            END,
+            'YYYY-MM-DD'
+        )
+        INTO v_StartDate
+        FROM KHMO
+        WHERE MAHP = :NEW.MAHP;
+    
+        -- Check if the current date is within 14 days of the semester start date
+        IF (v_CurrentDate - v_StartDate) > 14 THEN
+            -- If the current date exceeds the 14-day limit, raise an error for INSERT operations
+            IF INSERTING THEN
+                RAISE_APPLICATION_ERROR(-20001, 'You cannot add new registration after 14 days from the start of the semester.');
+            END IF;
+        ELSE
+            -- If the current date is within the limit, proceed with the insert or delete operation
+            IF INSERTING THEN
+                INSERT INTO DANGKY (MASV, MAGV, MAHP, HK, NAM, MACT, DIEMTHI, DIEMQT, DIEMCK, DIEMTK)
+                VALUES (:NEW.MASV, :NEW.MAGV, :NEW.MAHP, :NEW.HK, :NEW.NAM, :NEW.MACT, :NEW.DIEMTHI, :NEW.DIEMQT, :NEW.DIEMCK, :NEW.DIEMTK);
+            ELSIF DELETING THEN
+                DELETE FROM DANGKY
+                WHERE MASV = :OLD.MASV AND MAGV = :OLD.MAGV AND MAHP = :OLD.MAHP AND HK = :OLD.HK AND NAM = :OLD.NAM AND MACT = :OLD.MACT;
+            END IF;
+        END IF;
+    END IF;
+END;
+/
+
+
 -- CS4 : TRưởng Dơn vị
 -- view QLHS_TTCANHAN
 -- grant select on QLHS_PHANCONG_GD
@@ -161,43 +206,166 @@ END;
 -- Xem danh sách tất cả học phần (HOCPHAN), kế hoạch mở môn (KHMO) của chương
 -- trình Dào tạo mà sinh viên Dang theo học.
 create or replace function SVControl_XEMHP (P_SCHEMA VARCHAR2, P_OBJ VARCHAR2) 
-return varchar2
-as 
-    strsql varchar(2000); 
-begin
-    strsql:= 'HP.MACT = SV.MACT AND SV.MASV = SYS_CONTEXT(''USERENV'',''SESSION_USER'')';
-    return strsql;
-end;
+RETURN VARCHAR2 AS
+    CURSOR HP IS(select DK.MAHP from DANGKY DK, SINHVIEN SV where DK.MACT = SV.MACT AND SV.MASV = SYS_CONTEXT('USERENV','SESSION_USER') );
+    USERNAME VARCHAR2(128);
+    USERROLE VARCHAR2(128);
+    TEMP varchar2(5);
+    MAHP_DK varchar2(2000);
+BEGIN
+-- Lấy username của user hiện tại
+  USERNAME := SYS_CONTEXT('USERENV', 'SESSION_USER');
+  SELECT GRANTED_ROLE INTO USERROLE
+  FROM DBA_ROLE_PRIVS
+  WHERE GRANTEE = USERNAME;
+  IF 'RL_SINHVIEN' IN (USERROLE) THEN 
+      OPEN CUR;  
+        LOOP 
+            FETCH HP INTO TEMP; 
+            EXIT WHEN HP%NOTFOUND;
+            IF (MAHP_DK IS NOT NULL) THEN
+                MAHP_DK :=  MAHP_DK ||''',''';
+            ELSE 
+                MAHP_DK := MAHP_DK || TEMP;
+            END IF;
+        END LOOP;
+        CLOSE HP;
+        RETURN 'MAHP IN ('''||MAHP_DK||''')';
+  ELSE
+    RETURN '1=0';
+  END IF;
+END;
 BEGIN
     DBMS_RLS.ADD_POLICY(
-        object_schema => 'SYS', 
+        object_schema => 'ADPRO', 
         object_name => 'HOCPHAN',
-        policy_name => 'SVControl_XEMHP',
+        policy_name => 'SVControl_HOCPHAN_CS6',
         policy_function => 'SVControl_XEMHP',
         update_check => TRUE
     );
 END;
 
 create or replace function SVControl_XEMKHMO (P_SCHEMA VARCHAR2, P_OBJ VARCHAR2) 
-return varchar2
-as 
-    strsql varchar(2000); 
-begin
-    strsql:= 'KH.MACT = SV.MACT AND SV.MASV = SYS_CONTEXT(''USERENV'',''SESSION_USER'')';
-    return strsql;
-end;
+RETURN VARCHAR2 AS
+    CURSOR HP IS(select KH.MAHP from KHMO KH, SINHVIEN SV where KH.MACT = SV.MACT AND SV.MASV = SYS_CONTEXT('USERENV','SESSION_USER') );
+    USERNAME VARCHAR2(128);
+    USERROLE VARCHAR2(128);
+    TEMP varchar2(5);
+    MAHP_KH varchar2(2000);
+BEGIN
+-- Lấy username của user hiện tại
+  USERNAME := SYS_CONTEXT('USERENV', 'SESSION_USER');
+  SELECT GRANTED_ROLE INTO USERROLE
+  FROM DBA_ROLE_PRIVS
+  WHERE GRANTEE = USERNAME;
+  IF 'RL_SINHVIEN' IN (USERROLE) THEN 
+      OPEN CUR;  
+        LOOP 
+            FETCH HP INTO TEMP; 
+            EXIT WHEN HP%NOTFOUND;
+            IF (MAHP_KH IS NOT NULL) THEN
+                MAHP_KH :=  MAHP_KHMAHP_KH ||''',''';
+            ELSE 
+                MAHP_KH := MAHP_KHMAHP_KH || TEMP;
+            END IF;
+        END LOOP;
+        CLOSE HP;
+        RETURN 'MAHP IN ('''||MAHP_KH||''')';
+  ELSE
+    RETURN '1=0';
+  END IF;
+END;
 BEGIN
     DBMS_RLS.ADD_POLICY(
-        object_schema => 'SYS', 
+        object_schema => 'ADPRO', 
         object_name => 'KHMO',
-        policy_name => 'SVControl_XEMKHMO',
+        policy_name => 'SVControl_KHMO_CS6',
         policy_function => 'SVControl_XEMKHMO',
         update_check => TRUE
     );
 END;
-
 -- Thêm, Xóa các dòng dữ liệu Dăng ký học phần (DANGKY) liên quan Dến chính sinh
 --viên Dó trong học kỳ của năm học hiện tại (nếu thời Diểm hiệu chỉnh Dăng ký còn hợp
 --lệ).
+create or replace function SVControl_INSERT_DELETE_DANGKY (P_SCHEMA VARCHAR2, P_OBJ VARCHAR2) 
+RETURN VARCHAR2 AS
+    v_start_date DATE;
+    v_current_date DATE := SYSDATE;
+BEGIN
+    SELECT GRANTED_ROLE INTO USERROLE
+    FROM DBA_ROLE_PRIVS
+    WHERE GRANTEE = SYS_CONTEXT('USERENV', 'SESSION_USER');
+    IF 'RL_SINHVIEN' IN (USERROLE) THEN 
+        -- Get the start date of the semester for the current academic year
+        SELECT TO_DATE(
+                CASE 
+                    WHEN TO_CHAR(SYSDATE, 'MM') BETWEEN 1 AND 4 THEN TO_CHAR(SYSDATE, 'YYYY') || '-01-01'
+                    WHEN TO_CHAR(SYSDATE, 'MM') BETWEEN 5 AND 8 THEN TO_CHAR(SYSDATE, 'YYYY') || '-05-01'
+                    ELSE TO_CHAR(SYSDATE, 'YYYY') || '-09-01'
+                END,
+                'YYYY-MM-DD'
+            )
+        INTO v_start_date
+        FROM dual;
+    
+        -- Check if the current date is within the valid modification period (within the semester start date + 14 days)
+        IF (v_current_date - v_start_date) <= 14 THEN
+            RETURN '1=1'; -- Modification time is valid
+        ELSE RETURN '1=0';
+        END IF;
+    END IF;
+END;
+/
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema => 'ADPRO', 
+        object_name => 'KHMO',
+        policy_name => 'SVControl_DANGKY_INSERT_DELETE_CS6',
+        policy_function => 'SVControl_INSERT_DELETE_DANGKY',
+        STATEMENT_TYPES=>'DELETE, INSERT', 
+        update_check => TRUE
+    );
+END;
+--Sinh viên không được chỉnh sửa trên các trường liên quan đến điểm.
+create or replace function SVControl_UPDATE_DANGKY (P_SCHEMA VARCHAR2, P_OBJ VARCHAR2) 
+RETURN VARCHAR2 AS
+    USERNAME VARCHAR2(128);
+    USERROLE VARCHAR2(128);
+BEGIN
+-- Lấy username của user hiện tại
+  USERNAME := SYS_CONTEXT('USERENV', 'SESSION_USER');
+  SELECT GRANTED_ROLE INTO USERROLE
+  FROM DBA_ROLE_PRIVS
+  WHERE GRANTEE = USERNAME;
+  IF 'RL_SINHVIEN' IN (USERROLE) THEN 
+    RETURN '1=0';
+  END IF;
+END;
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema => 'ADPRO', 
+        object_name => 'KHMO',
+        policy_name => 'SVControl_KHMO_CS6',
+        policy_function => 'SVControl_XEMKHMO',
+        statement_types=>'UPDATE', 
+        sec_relevant_cols => 'DIEMTHI,DIEMQT,DIEMCK,DIEMTK',
+        sec_relevant_cols_opt => dbms_rls.ALL_ROWS,
+        update_check => TRUE
+    );
+END;
+-- Sinh viên được Xem tất cả thông tin trên quan hệ ĐANGKY tại các dòng dữ liệu liên
+-- quan đến chính sinh viên.
+/
+BEGIN
+    DBMS_RLS.ADD_POLICY(
+        object_schema => 'ADPRO', 
+        object_name => 'SINHVIEN',
+        policy_name => 'SVControl_SELECT_DANGKY',
+        policy_function => 'SVControl_XEMTTCN',
+        statement_types => 'SELECT, UPDATE',
+        update_check => TRUE
+    );
+END;
+/
 ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE;
     
